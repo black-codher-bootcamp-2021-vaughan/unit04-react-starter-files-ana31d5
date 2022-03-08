2370e577cf1561064be0428b3217bdf446dc4189
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-03-08 13:58:44 +0000

    task1 & 2

--- session01/index.js
+++ session01/index.js
@@ -1,1 +1,21 @@
-// Add the code here
+function ClickButton() {
+
+    const [clicked, setClick] = React.useState(false);
+  
+    if (clicked) {
+      return React.createElement(
+      'h1', {className : 'title'}, 'Hello World!' ) 
+    }
+  
+    return React.createElement( 
+        'button', {
+            className: 'main-button',
+    
+      onClick: () => setClick(true)
+    },
+      'Press Here'
+    );
+  }
+  
+  const domContainer = document.querySelector('#root');
+  ReactDOM.render(React.createElement(ClickButton), domContainer);
